9bc676f0d0bd7cbd3d4b10245b65e38e10f08931
Catherine Salazar <catcreates13@gmail.com>
2018-01-23 05:11:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(long_words)
 end
 
 def find_the_cheese(snacks)
-cheese_types = ["cheddar", "gouda", "camembert"]
+
 
 if cheese_types.include?(snacks) {|snack| put "#{snack}"}
 else
