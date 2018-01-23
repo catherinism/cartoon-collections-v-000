bf797c67ddd2792149f271660051d661c4e9835d
Catherine Salazar <catcreates13@gmail.com>
2018-01-23 05:27:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,3 +25,11 @@ snacks.find do |cheese|
   cheese.include?(cheese)
 end
   end
+
+  def find_the_cheese(potentially_cheesy_items)
+    cheeses = %w(gouda cheddar camembert)
+
+    potentially_cheesy_items.find do |maybe_cheese|
+      cheeses.include?(maybe_cheese)
+    end
+  end
