959ac7587e6b0f6d972893060dffff756f05181c
Catherine Salazar <catcreates13@gmail.com>
2018-01-23 05:10:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(snacks)
 cheese_types = ["cheddar", "gouda", "camembert"]
 
-if snacks.include?(snacks){|snack| put "#{snack}"}
+if cheese_types.include?(snacks) {|snack| put "#{snack}"}
 else
   nil
 end
