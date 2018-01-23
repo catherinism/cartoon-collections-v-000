43ad7c872ce5cbae2221a7182844d6a1f71d6b68
Catherine Salazar <catcreates13@gmail.com>
2018-01-23 05:26:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,15 +21,8 @@ end
 def find_the_cheese(snacks)
 cheese_types = ["cheddar", "gouda", "camembert"]
 
-snacks.find do |cheese| 
+snacks.find do |cheese|
   cheese.include?(cheese)
 end
   end
 
-  def find_the_cheese(potentially_cheesy_items)
-    cheeses = %w(gouda cheddar camembert)
-
-    potentially_cheesy_items.find do |maybe_cheese|
-      cheeses.include?(maybe_cheese)
-    end
-  end
