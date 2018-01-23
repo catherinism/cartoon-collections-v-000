66075a417aac55cad69b9edc9137fa186f1096dc
Catherine Salazar <catcreates13@gmail.com>
2018-01-23 05:25:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,9 +18,10 @@ def long_planeteer_calls(long_words)
   end
 end
 
-def find_the_cheese(snacks)
-cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w(gouda cheddar camembert)
 
-snacks.find do |cheese| cheese.include?(cheese)
-end
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
+end
